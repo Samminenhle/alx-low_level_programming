52176ecceac20cffb5a7997bca295054841c0c09
samminenhle <samminenhle@gmail.com>
2022-10-17 18:35:00 -0700

    p/n

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,7 +12,7 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAXI / 2;
+	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
@@ -20,7 +20,7 @@ int main(void)
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n*, n");
+		printf("%d is zero \n*, n");
 	}
 	else 
 	{
