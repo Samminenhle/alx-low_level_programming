fe6bdc70f4d73e7c0f2d83c13045e360cab5288a
samminenhle <samminenhle@gmail.com>
2022-10-18 20:05:24 -0700

    alp

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+
+/**
+ * main - prints the alphabets in lower cases
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char la;
+	for (la = 'a'; la <= 'z'; la++
+
+		_putchar(la);
+
+	_putchar('\n');
+}
+
