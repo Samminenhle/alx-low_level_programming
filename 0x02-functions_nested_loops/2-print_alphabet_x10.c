04e1884d70d8a84727c395a331861b53f8141ff4
samminenhle <samminenhle@gmail.com>
2022-10-19 15:31:01 -0700

    print

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,10 +9,10 @@
  */
 void print_alphabet_x10(void)
 {
-	int 10
-	char la
+	int ten;
+	char la;
 
-	for (10 = 0; 10 <= 9; 10++)
+	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
