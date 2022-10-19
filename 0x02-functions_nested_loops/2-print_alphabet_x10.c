a80bfb2d57226613e918e6280504c80e4a0ea683
samminenhle <samminenhle@gmail.com>
2022-10-19 15:56:52 -0700

    print alp

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - prints 10x the alphabets in lower case
+ * main -  print_alphabet_x10
  *
  */
 void print_alphabet_x10(void)
@@ -9,11 +9,10 @@ void print_alphabet_x10(void)
 	int ten;
 	char la;
 	
-	for (ten = 0; ten <=9; 10++)
+	for (ten = 0; ten <= 9; ten++)
 	{
 		for (la = 'a'; la = 'z'; la++)
 			_putchar(la);
 		_putchar('\n');
-
 	}
 }
