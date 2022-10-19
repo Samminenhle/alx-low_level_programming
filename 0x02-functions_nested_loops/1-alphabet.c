a0bca8bb3f389467bae2e02c35f7d8afb731e2e5
samminenhle <samminenhle@gmail.com>
2022-10-19 12:28:50 -0700

    alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,12 @@
 /**
  * main - prints the alphabets in lower cases
  *
- * Return: Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char la;
-	for (la = 'a'; la <= 'z'; la++);
+
+	for (la = 'a'; la <= 'z'; la++)
 		_putchar(la);
 
 	_putchar('\n');
