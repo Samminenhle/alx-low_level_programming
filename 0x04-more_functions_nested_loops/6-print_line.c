4439bfddc096424287f5fe993dfa1dab73494344
samminenhle <samminenhle@gmail.com>
2022-10-21 15:36:41 -0700

    prnt

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,1 +1,20 @@
 #include "main.h"
+
+/**
+ * print_line - draws a straight line in the terminal
+ *
+ * @n: the number of '_' to be printed
+ * Rertun: Always 0
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+
+	_putchsr('\n');
+}
