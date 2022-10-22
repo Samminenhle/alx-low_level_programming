854afa27d2414947e3d8d988ea5f85db1665132f
samminenhle <samminenhle@gmail.com>
2022-10-22 00:33:43 -0700

    p

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,15 +5,16 @@
  *
  * Return: Always 0
  */
+
 void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = a; a <= 9; a++)
 	{
 		for (num = 0; num <= 14; num++)
 		{
-			if (num > 9)
+			if (num >= 9)
 			{
 
 				_putchar((num / 10) + '0');
