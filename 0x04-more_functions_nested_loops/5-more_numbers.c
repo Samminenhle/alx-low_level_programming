cea69182c312f59bfc81b2c4afe7f2f02b94da9e
samminenhle <samminenhle@gmail.com>
2022-10-22 01:40:18 -0700

    P

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,20 +8,15 @@
 
 void more_numbers(void)
 {
-	int num, a;
-
-	for (a = 0; a <= 9; a++)
-	{
-		for (num = 0; num <= 14; num++)
+	int a, b;
+		for (a = 1; a <= 10; a++)
 		{
-			if (num >= 9)
+			for (b = 0; b <= 14; b++)
 			{
-
-				_putchar((num / 10) + '0');
+				if (b >= 10)
+				_putchar('1');
+				_putchar(b % 10 + '0');
 			}
-			-putchar((num % 10) + '0');
+				_putchar('\n');
 		}
-
-		_putchar('\n');
-	}
 }
