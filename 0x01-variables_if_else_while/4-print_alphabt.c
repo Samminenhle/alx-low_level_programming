3db4ed18dc77f6649810843dc55da7bc06ae3a49
samminenhle <samminenhle@gmail.com>
2022-10-23 19:09:00 -0700

    p

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,3 +7,15 @@
  */
 int mai(void)
 {
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
