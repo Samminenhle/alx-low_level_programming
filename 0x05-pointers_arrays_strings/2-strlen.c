28f5a27387e92b5284ea07397aed216688e5710a
samminenhle <samminenhle@gmail.com>
2022-10-24 15:52:02 -0700

    2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * int _strlen - returns the length of a string
+ *@s: character to check
  *
- * Return: Always 0
- *
+ * Return: length of a string
  */
 int _strlen(char *s)
 {
+	int a = 0;
+
+	for (; *s++;)
+		a++;
+	return (a);
+}
