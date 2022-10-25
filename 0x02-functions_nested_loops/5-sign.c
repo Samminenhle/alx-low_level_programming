4283c22db02c21e1151142ca71d032a445384090
samminenhle <samminenhle@gmail.com>
2022-10-24 19:00:39 -0700

    p

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,11 @@
 
 /**
  * print_sign(int n) - write the sign of a number
- *
  * @n: the sign of a number
- * Return: 1 if n is greater than 0
  *
- * 0 if n is zero, and -1 if n is less than zero
+ * Return: 1 if n is greater than 0
+ * 0 if n is zero, and
+ * -1 if n is less than zero
  */
 int print_sign(int n)
 {
