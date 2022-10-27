09e90e96d5e020c2877eeec17916048af0beda5d
samminenhle <samminenhle@gmail.com>
2022-10-27 14:01:42 -0700

    5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,1 +1,22 @@
 #include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @n: string
+ *
+ * Return: character
+ */
+char *string_toupper(char *)
+{
+	int a;
+
+	a = 0;
+	while (n[a] != '\0')
+	{
+		if (n[a] >= 'a' && n[a] <= 'z')
+			n[a] = n[a] - 32;
+		a++;
+	}
+	return (n);
+}
+
