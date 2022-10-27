9b8a786283fdf4bd54ce1534c3ad6d62bc05b25a
samminenhle <samminenhle@gmail.com>
2022-10-27 12:49:59 -0700

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,1 +1,24 @@
 #include "main.h"
+
+/**
+ *  _strcmp - compares two strings
+ *  @s1: string 1
+ *  @s2: string 2
+ *
+ *  Return: Always 0
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int a;
+
+	a = 0;
+	while (s1[a] != '\0')
+	{
+		if (s1[a] != s2[a])
+		{
+			return (s1[a] - s2[a]);
+		}
+		a++;
+	}
+	return (0);
+}
