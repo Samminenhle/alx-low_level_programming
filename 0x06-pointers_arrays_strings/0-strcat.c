98e25ac5c3fee7131cb867c9a4a454b454db09a2
samminenhle <samminenhle@gmail.com>
2022-10-27 11:41:39 -0700

    0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,21 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	char *s = dest;
+	int a
+	int b
 
-	while (*dest != '\0')
+	a = 0
+	while (dest[a] != '\0')
 	{
-		dest++;
+		a++;
 	}
-
-	while (*src != '\0')
+	b = 0;
+	while (src[b] != '\0')
 	{
-		*dest = *src;
-		dest++;
-		src++;
+		dest[a] = src[b];
+		a++;
+		b++;
 	}
-	*dest = '\0';
-	return (s);
+	dest[a] = '\0';
+	return (dest);
 }
