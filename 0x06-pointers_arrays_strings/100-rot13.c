63e76b5036e5007e242fe53cc3b47301950354d6
samminenhle <samminenhle@gmail.com>
2022-10-30 18:40:33 -0700

    100

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13.
+ * @s: string to encode
+ *
+ * Return: Always 0
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char a[] = "ij"
+		char b[] = "s"
+
+		for (i = 0; *(s + i); i++)
+		{
+			for (j = 0; j < 52; j++)
+			{
+				if (a[j] == *(s + i))
+				{
+					*(s + i) = b[j];
+					break;
+				}
+			}
+		}
+	return (s);
+}
