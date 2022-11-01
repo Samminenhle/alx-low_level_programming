6d7767dc1b87d9cd5dad4dbc215e1c01f7574503
samminenhle <samminenhle@gmail.com>
2022-10-31 18:42:00 -0700

    3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,8 +2,28 @@
 
 /**
  * _strspn - searches a string for any of a set of bytes.
+ * @s: the initial segment of s
+ * @accept: bytes from
  *
- *
- * Return: Always 0
+ * Return: the number of bytes in the initial segment of s
+ * which consist only of bytes from accept
  */
 unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+	return (z);
+}
