6d7c9deab4404af9e879876adcf66de359513bf7
samminenhle <samminenhle@gmail.com>
2022-10-31 20:09:12 -0700

    7

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -2,9 +2,21 @@
 
 /**
  * print_chessboard - prints the chessboard
- *
+ * @a: pointer to print
  *
  * Return: Always 0
  *
  */
 void print_chessboard(char (*a)[8])
+{
+	int s, t;
+
+	for (s = 0; s < 8; s++)
+	{
+		for (t = 0; t < 8; t++)
+		{
+			_putchar(a[s][t]);
+		}
+		_putchar('\n');
+	}
+}
