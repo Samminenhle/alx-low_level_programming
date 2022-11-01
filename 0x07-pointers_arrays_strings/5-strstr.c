89169006842a6df7b1a9375aa366552c67e312ad
samminenhle <samminenhle@gmail.com>
2022-10-31 19:33:23 -0700

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -2,9 +2,32 @@
 
 /**
  * _strstr - locates a substring
+ * @needle: first occurrence of the substring
+ * @haystack:  in the string
  *
- *
- * Return: Always 0
+ * Return: a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found
  */
 char *_strstr(char *haystack, char *needle)
+{
+	unsigned int a = 0, b = 0;
 
+	while (haystack[a])
+	{
+		while (needle[b] && (haystack[a] == needle[0]))
+		{
+			if (haystack[a + b] == needle[b])
+				b++;
+			else
+				break;
+		}
+		if (needle[b])
+		{
+			a++;
+			b = 0;
+		}
+		else
+			return (haystack + a);
+	}
+	return (0);
+}
