7945f589714b55e88a9967f5a689d60aadeda913
samminenhle <samminenhle@gmail.com>
2022-10-31 18:28:04 -0700

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,9 +2,24 @@
 
 /**
  * _strchr - locates a character in a string.
+ * @c: first occurrence of the character c
+ * @s: the string s
  *
- *
- * Return: Always 0
+ * Return: a pointer to the first occurrence of the character c
+ * in the string s
+ * or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
 
+	return (NULL);
+}
