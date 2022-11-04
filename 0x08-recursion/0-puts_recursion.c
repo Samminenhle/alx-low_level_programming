2f95bee2127a0bb01de04a45d1ed056e45d90e6a
samminenhle <samminenhle@gmail.com>
2022-11-03 17:44:43 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string
+ *
+ * @s: strings
+ * Return: Always 0
+ */
+
+void _puts_recursion(char *s)
+{
+	is (*s)
+
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
