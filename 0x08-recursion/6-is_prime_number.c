42a6526749846f17a780de519405770b6357fdd8
Samminenhle <samminenhle@gmail.com>
2022-11-07 13:24:50 +0000

    prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -19,7 +19,7 @@ int scan(int a, int b)
 }
 
 /**
- * is _prime_number - states if the input integer is a prime number
+ * is_prime_number - states if the input integer is a prime number
  * @n: integer
  *
  * Return: returns 1 if the input integer is a prime number,
@@ -30,5 +30,5 @@ int is_prime_number(int n)
 {
 	if (n == 2)
 		return (1);
-	return (check(2, n));
+	return (scan(2, n));
 }
