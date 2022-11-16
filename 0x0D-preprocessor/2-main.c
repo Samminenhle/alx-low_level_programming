61a841541c4e8413a8393ababacf5a651666fbbf
Samminenhle <samminenhle@gmail.com>
2022-11-16 03:53:29 +0000

    p

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it was compiled from
+ * samminenhle
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	Iprintf("%s\n",_FILE_);	
+		return (0);
+}
