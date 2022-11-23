ff3832a1ac89977157432473784c031f2e88e8d3
Samminenhle <samminenhle@gmail.com>
2022-11-23 20:13:23 +0000

    funct

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,1 +1,11 @@
 #include <main.h>
+
+/**
+ * _calloc - allocates memory for an array, using malloc 
+ * of nmemb elements of size bytes each and returns a pointer
+ * to the allocated memory.
+ * @size: allocate the element of size bytes
+ * @nmemb: allocate the memmory for an array
+ *
+ * Return: NULL if nmemb or size is 0,
+ * NULL if malloc fails`
