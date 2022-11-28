03e574314018341f41f08ddf1f4b357be7839d1e
Samminenhle <samminenhle@gmail.com>
2022-11-28 19:20:49 +0000

    linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
- * @h: head
+ * print_listint - Prints all the elements of a listint_t list
+ * @h: Head
  *
- * Return: the number of nodes
+ * Return: The number of nodes
  */
 
 size_t print_listint(const listint_t *h);
--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * listint_len - returns the number of elements in a linked listint_t list.
- * @h: head pointer
+ * listint_len - Returns the number of elements in a linked listint_t list.
+ * @h: Head pointer
  *
- * Return: returns the number of elements in a linked listint_t list.
+ * Return: Returns the number of elements in a linked listint_t list.
  */
 
 size_t listint_len(const listint_t *h);
