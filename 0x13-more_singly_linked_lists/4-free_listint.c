8fb9cfae0bd6e4b00aef9f39a38d901624b75a96
Samminenhle <samminenhle@gmail.com>
2022-11-29 08:06:46 +0000

    singly linked lists

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: a ponter to the head struct of linked list
+ *
+ * Return: 0
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	if (head == NULL)
+		return;
+	for (temp = head; temp; temp = temp->next)
+		free(temp);
+}
+
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,2 +1,23 @@
 #include "lists.h"
 
+/**
+ * get_nodeint_at_index - returns the nth node of a listint_t linked list
+ * @head: a pointer to the beginning struct of the linked list
+ * @index: the index of the node to be returned
+ *
+ * Return: the pointer the number index structure of the linked linked
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; head && (i < index); head = head->next, i++)
+		;
+	if (head == NULL)
+		return (NULL);
+	return (head);
+}
+
