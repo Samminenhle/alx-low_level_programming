11bc8f5af641cb0650d7d7383f1a18c229882364
Samminenhle <samminenhle@gmail.com>
2022-12-09 03:12:25 +0000

    file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -13,17 +13,17 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd;
+	int file_descriptor;
 	ssize_t len;
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
+	file_descriptor = open(filename, O_WRONLY | O_APPEND);
+	if (file_descriptor == -1)
 		return (-1);
 	if (text_content != NULL)
 		len = write(fd, text_content, _strlen(text_content));
-	close(fd);
+	close(file_descriptor);
 	if (len == -1)
 		return (-1);
 	return (1);
@@ -31,15 +31,16 @@ int append_text_to_file(const char *filename, char *text_content)
 
 /**
  * _strlen - finds the length of a string
- * @str: string
+ * @s: string
  *
  * Return: length of the string
  */
-size_t _strlen(char *str)
-{
-	size_t i;
 
-	for (i = 0; str[i]; i++)
-		;
-	return (i);
+int _strlen(const char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	return (len);
 }
