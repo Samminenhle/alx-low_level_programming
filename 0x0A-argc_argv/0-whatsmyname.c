d80f3c0cf4883b693bd9fff0961b3dbd6a2b1967
Samminenhle <samminenhle@gmail.com>
2023-06-25 10:25:12 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - print name 
+ * @argc: argument v
+ *
+ * Return: void
+ */
+
+int main(int arg, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
