7212750e26f35229ab97934b67463d8bd4009ff8
Samminenhle <samminenhle@gmail.com>
2023-08-16 15:39:32 -0700

    function_poiters

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,18 +1,17 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef FILE_CALC
+#define FILE_CALC
 
 /**
- * struct op - struct op
- * @op: operator
- * @f: function
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
  */
-
 typedef struct op
 {
-        char *op;
-        int (*f)(int a, int b);
-}
-op_t;
+	char *op;
+	int (*f)(int a, int b);
+} op_t;
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,34 +1,26 @@
-#include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
-
+#include "3-calc.h"
 
 /**
- * main - function that prints the result of the operation
- * @argc: argument count
- * @argv: araguments
- *
- * Return:
+ * main - main file
+ * @argc: number of lines arguments
+ * @argv: array of elements
+ * Return: 0
  */
 
 int main(int argc, char *argv[])
 {
-        int (*oprt)(int, int);
+	int R;
 
-        if (argc != 4)
-        {
-                printf("Error\n");
-                exit(98);
-        }
-
-        oprt = get_op_func(argv[2]);
-
-        if (!oprt)
-        {
-                printf("Error\n");
-                exit(99);
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
 	}
 
-	printf("%d\n", oprt(atoi(argv[1]), atoi(argv[3])));
-        return (0);
+	R = (*get_op_func(argv[2]))(atoi(argv[1]), atoi(argv[3]));
+	printf("%d\n", R);
+
+	return (0);
 }
