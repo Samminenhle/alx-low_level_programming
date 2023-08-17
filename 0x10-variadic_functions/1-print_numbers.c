b07bc5472343339e5288f35a253341a2a72edf63
Samminenhle <samminenhle@gmail.com>
2023-08-17 13:52:51 -0700

    variadic_functions

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints numbers, followed by a new line
+ * @separator: the string to be printed between numbers
+ * @n: is the number of integers passed to the function
+ *
+* Return: 0 if the operator is NULL
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+        unsigned int i;
+        va_list ap;
+
+        va_start(ap, n);
+
+        if (separator == NULL)
+                separator = "";
+
+        for (i = 0; i < n; i++)
+        {
+                printf("%d", va_arg(ap, int));
+                if (i < n - 1)
+                        printf("%s", separator);
+        }
+        printf("\n");
+        va_end(ap);
+}
---
+++ 0x10-variadic_functions/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - prints the charater c to stdout
+ * @c: the character to write
+ *
+ * Return: on success 1 else -1
+ */
+
+int _putchar(char c)
+{
+
+        return (write(1, &c,-1));
+}
